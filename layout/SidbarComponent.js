02c78cbb4d591c5142f1e37bacf093ead8fb19b0
piraji <piraji.s@whizit.co.in>
2023-08-15 13:02:39 +0530

    changes

--- layout/SidbarComponent.js
+++ layout/SidbarComponent.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { Heart } from 'bootstrap-icons';
+// import { Heart } from 'bootstrap-icons';
 
 import 'bootstrap/dist/css/bootstrap.css'
+import Link from 'next/link'
 
 
 const sideLabel = [
     {
       id: 1,
       name: "Dashboard",
-      icon: <Heart />,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-dash" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M5.5 6.5A.5.5 0 0 1 6 6h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
+      <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+    </svg>,
       link: '/'
+    },
+    {
+      id: 2,
+      name: "Total student",
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-hearts" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"/>
+    </svg>,
+      link: '/student'
     }
     // {
     //   id: 2,
@@ -34,19 +46,14 @@ const sideLabel = [
 const SidbarComponent = () => {
 
   return (
-  <div className='   d-flex flex-row justify-content-start' style={{height:"100vh"}} >
-    {/* <SideBarComponent/> */}
-    <div className='px-4 pt-8 pb-4 bg-ligth d-flex justify-content-between  flex-col border  border-end-0  w-80'  style={{height:"100vh"}}>
-{sideLabel.map((item,index)=>{
-  return(
-    <div key={index}>
-      <h3 >{item.icon}{item.name}</h3>
-    </div>
-  )
-})}
-
-    </div>
-    {/* <div className='  p-4 text-white border border-start-0'></div> */}
+  <div className='d-flex flex-row justify-content-start' style={{height:"100vh"}} >
+        {sideLabel.map((item,index)=>{
+          return(
+            <Link key={index} href={item.link} >
+              <div >{item.icon}<label> {item.name} </label></div>
+             </Link>
+          )
+        })}
   </div>
   )
 }
